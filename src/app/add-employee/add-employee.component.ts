c198ac0c19a260e23f17c17a1e6f885fa71260f3
Irvin Gil <irvin.mercado@synacy.com>
2022-09-01 21:51:40 +0800

    feat:<not-yet-working> create base implmentation code for sending data from non-related components

--- src/app/add-employee/add-employee.component.ts
+++ src/app/add-employee/add-employee.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+
 
 @Component({
   selector: 'app-add-employee',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddEmployeeComponent implements OnInit {
 
-  constructor() { }
+
+  constructor() {
+  }
 
   ngOnInit(): void {
+
   }
 
 }
