656c2c9a773b2b4dffcf617c822746dfc5b5fb84
Irvin Gil <irvin.mercado@synacy.com>
2022-09-02 09:48:32 +0800

    feat: integrate sidebar and homepage components and perform data communication using shared-service

--- src/app/shared.service.ts
+++ src/app/shared.service.ts
@@ -1,62 +1,22 @@
 import {Injectable} from '@angular/core';
-import {BehaviorSubject} from "rxjs";
+import {BehaviorSubject, Subject} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class SharedService {
-  private receiveEmployeeId = new BehaviorSubject(0);
-  employeeId = this.receiveEmployeeId.asObservable();
+  private employeeSource = new BehaviorSubject(
+    {id: 0, name: '', role: '', manager: 0, annualLeave: 0});
+  chosenEmployee = this.employeeSource.asObservable();
 
-  private receiveEmployeeName = new BehaviorSubject('');
-  employeeName = this.receiveEmployeeName.asObservable();
-
-  private receiveEmployeeRole = new BehaviorSubject('');
-  employeeRole = this.receiveEmployeeRole.asObservable();
-
-  private receiveEmployeeManager = new BehaviorSubject(0);
-  employeeManager = this.receiveEmployeeManager.asObservable();
-
-  private receiveEmployeeLeave = new BehaviorSubject(0);
-  employeeLeave = this.receiveEmployeeLeave.asObservable();
 
   constructor() {
   }
 
-  changeEmployeeId(id: number) {
-    this.receiveEmployeeId.next(id);
+  changeEmployee(value: any) {
+    this.employeeSource.next(value);
   }
 
-  changeEmployeeName(name: string) {
-    this.receiveEmployeeName.next(name);
-  }
-
-  changeEmployeeRole(role: string) {
-    this.receiveEmployeeRole.next(role);
-  }
-
-  changeEmployeeManager(manager: number) {
-    this.receiveEmployeeManager.next(manager);
-  }
-
-  changeEmployeeLeave(leave: number) {
-    this.receiveEmployeeLeave.next(leave);
-  }
 }
 
-// import { Injectable } from '@angular/core';
-// import { BehaviorSubject } from 'rxjs';
-//
-// @Injectable()
-// export class DataService {
-//
-//   private messageSource = new BehaviorSubject('default message');
-//   currentMessage = this.messageSource.asObservable();
-//
-//   constructor() { }
-//
-//   changeMessage(message: string) {
-//     this.messageSource.next(message)
-//   }
-//
-// }
+
