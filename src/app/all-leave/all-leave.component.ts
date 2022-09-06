e29a7f7a469b8069a30e9c89241fdaa7cacba30c
KimJohnAB <kim.bautista@synacy.com>
2022-09-06 13:36:34 +0800

    View all leave admin side implementation

--- src/app/all-leave/all-leave.component.ts
+++ src/app/all-leave/all-leave.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {AllLeaveService} from "./all-leave.service";
 
 @Component({
   selector: 'app-all-leave',
@@ -6,10 +7,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./all-leave.component.css']
 })
 export class AllLeaveComponent implements OnInit {
+  public leaveList;
+  public loading;
+  public hasError;
 
-  constructor() { }
+  constructor(private allLeaveService: AllLeaveService ) { }
 
   ngOnInit(): void {
+    this.getLeaveInfo();
+    console.log(this.leaveList);
+
+  }
+
+  getLeaveInfo(){
+    this.loading = true;
+    this.allLeaveService.getLeaves().subscribe((response) =>{
+      this.leaveList=response.content;
+
+      console.log(response);
+    }, error => {
+      console.log(error)
+    }).add(()=>{
+      this.loading=false;
+    });
   }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import {HttpClientModule} from "@angular/common/http";
 import {FormsModule} from "@angular/forms";
 import {CommonModule} from "@angular/common";
 import {HomepageComponent} from "./homepage/homepage.component";
+import {AllLeaveModule} from "./all-leave/all-leave.module";
 
 
 
@@ -31,6 +32,7 @@ import {HomepageComponent} from "./homepage/homepage.component";
     AppRoutingModule,
     AddEmployeeModule,
     AllEmployeesModule,
+    AllLeaveModule,
     AllLeaveRoutingModule,
     HomepageRoutingModule,
     ApplyLeaveRoutingModule,
